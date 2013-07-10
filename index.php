7a37ac20244c3d91edf3d678fb62bc58a39c723d
gersonj13 <gersonr13@gmail.com>
2013-07-10 16:45:47 -0430

    Correccion Flecha Carousel

--- index.php
+++ index.php
@@ -169,8 +169,8 @@
                                         </div>
                                     </div>
                                     <!-- Carousel nav -->
-                                    <a class="carousel-control left" href="#myCarousel" data-slide="prev">‹</a>
-                                    <a class="carousel-control right" href="#myCarousel" data-slide="next">›</a>
+                                    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                                    <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
                                 </div>
 
                                 <div style="margin-bottom:5px"> 
