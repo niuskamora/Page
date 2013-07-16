84a6702bcdd404fe3f21ab7a275e7d3a90526845
gersonj13 <gersonr13@gmail.com>
2013-07-16 11:30:02 -0430

    modificacion Front End

---
+++ recursos/js/funciones.js
@@ -0,0 +1,74 @@
+$(function () {
+	$('#ca-container').contentcarousel();
+    $(window).scroll(function(){
+        // add navbar opacity on scroll
+        if ($(this).scrollTop() > 100) {
+            $(".navbar.navbar-fixed-top").addClass("scroll");
+        } else {
+            $(".navbar.navbar-fixed-top").removeClass("scroll");
+        }
+
+        // global scroll to top button
+        if ($(this).scrollTop() > 300) {
+            $('.scrolltop').fadeIn();
+        } else {
+            $('.scrolltop').fadeOut();
+        }        
+    });
+	 // scroll back to top btn
+    $('.scrolltop').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 700);
+        return false;
+    });
+	 $('.carousel').carousel({
+                    interval: 3000,
+					
+                });
+		$('.carousel').carousel('cycle');
+      });
+            $(document).ready(function() {
+                magicline();
+            });
+
+            function magicline() {
+
+                var $el, leftPos, newWidth,
+                        $mainNav = $("#nav");
+                $("#magic-line").remove();
+                $mainNav.append('<li id="magic-line"></li>');
+                var $magicLine = $('#magic-line');
+
+                $magicLine
+                        .width($('#nav').find('.active').width())
+                        .css('left', $('#nav').find('.active a').position().left)
+                        .data('origLeft', $magicLine.position().left)
+                        .data('origWidth', $magicLine.width());
+
+                $('#nav li a').hover(function() {
+
+                    $el = $(this);
+
+                    leftPos = $el.position().left;
+                    if ($el.closest('li.dropdown').size() != 0)
+                        leftPos += $el.closest('li.dropdown').position().left;
+                    newWidth = $el.parent().width();
+                    $magicLine.stop().animate({
+                        left: leftPos,
+                        width: newWidth
+                    });
+                }, function() {
+                    $magicLine.stop().animate({
+                        left: $magicLine.data('origLeft'),
+                        width: $magicLine.data('origWidth')
+                    });
+                });
+            }
+            $('li a').on('click', function() {
+                $('li').each(function() {
+                    $(this).removeClass('active');
+
+                });
+                $(this).parent().addClass('active');
+                magicline();
+	
+});
