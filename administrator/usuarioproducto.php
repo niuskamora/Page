f641768fb8577c52b0cc33b88b7cc7d93ca23034
Mariela Arboleda <luz.arboleda25@gmail.com>
2013-07-25 15:57:38 -0430

    ok

--- administrator/usuarioproducto.php
+++ administrator/usuarioproducto.php
@@ -6,6 +6,26 @@ $conn=conectar();
 if(!isset($_SESSION["usuarioadmin"]) || !isset($_SESSION["passwordadmin"])){
 	iraURL('../administrator/index.php');
 	}
+
+		
+if(isset($_POST["guardar"])){
+	
+	if($_POST["producto"]>0){
+		
+		$nombre=$_POST['nombre'];
+		$submenu=$_GET["id"];
+		$admin=$_SESSION["id_usuario"];
+		$enla=$_POST['enlace'];
+	$resultado=pg_query($conn,"INSERT INTO menu values( nextval('menu_menuid_seq'),'$nombre','$submenu','$admin','$enla')") or die(pg_last_error($conn));
+	
+	if($resultado){
+			
+			llenarLog(1, "Creo submenu");
+			iraURL('../administrator/submenu.php?id='.$_GET['id']);
+		}
+	}else{
+		javaalert("Debe llenar todos los campos obligatorios");
+}
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +38,6 @@ if(!isset($_SESSION["usuarioadmin"]) || !isset($_SESSION["passwordadmin"])){
 <meta charset="utf-8">
 <link href="../recursos/css/bootstrap.css" rel="stylesheet">
 <link href="../recursos/css/bootstrap.min.css" rel="stylesheet">
-<link href="../recursoscss/bootstrap-responsive.css" rel="stylesheet">
 <link href="../recursos/css/bootstrap-responsive.min.css" rel="stylesheet">
 <link href="../recursos/css/estiloadmin.css" rel="stylesheet">
 <link href="../recursos/footable/css/footable-0.1.css" rel="stylesheet" type="text/css" />
@@ -32,7 +51,7 @@ if(!isset($_SESSION["usuarioadmin"]) || !isset($_SESSION["passwordadmin"])){
     <div class="navbar-inner">
       <div class="container" style="width: auto;"> <a class="btn btn-navbar" href="#nav" data-toggle="collapse" data-target="#barrap"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a> <a  class="brand" id="brand-admin" href="principal.php">PANGEATECH</a>
         <div id="barrap" class="nav-collapse collapse">
-         <ul class="nav">
+          <ul class="nav">
             <li class="dropdown active"> <a  class="dropdown-toggle" data-target="#" data-toggle="dropdown"> Gestión Usuarios <b class="caret"></b> </a>
               <ul class="dropdown-menu">
                 <li><a href="tipoadmin.php"> Tipo Administrador </a></li>
@@ -40,7 +59,7 @@ if(!isset($_SESSION["usuarioadmin"]) || !isset($_SESSION["passwordadmin"])){
                 <li><a href="usuario.php">Usuario</a></li>
               </ul>
             </li>
-            <li><a href="menu.php"> Menú</a></li>
+            <li ><a href="menu.php"> Menú</a></li>
             <li><a href="producto.php">Producto</a></li>
             <li><a href="sucursal.php">Sucursal</a></li>
             <li class="dropdown">
@@ -65,94 +84,128 @@ if(!isset($_SESSION["usuarioadmin"]) || !isset($_SESSION["passwordadmin"])){
    <div class="row-fluid">
                        
     <div class="span3">
-          <div style="text-align:center">
+      <div style="text-align:center">
         
-          <ul class="nav  nav-pills nav-stacked">
-              <li class="active"><a href="crearusuario.php"> <span class="add-on"><i class="icon-plus"></i></span> Crear </a></li>
-              <li><a href="principal.php"> <span class="add-on"><i class="icon-arrow-left"></i></span> Atrás</a></li>
-            
+                 <ul class="nav  nav-pills nav-stacked">
+              <li class="active"><a href="usuario.php"> <span class="add-on"><i class="icon-arrow-left"></i></span> Atrás </a></li>
+
           </ul>
-          
+
 
         
       </div>
     </div>
-    <div class="span9">
-      <?php 
-		$SQL="SELECT * FROM usuario";
-		$result = pg_query ($conn, $SQL ) or die("Error en la consulta SQL");
-		$registros= pg_num_rows($result);		
-	if($registros == 0){
-    ?>
-    <div class="alert alert-block" align="center">
-   			<h2 style="color:rgb(255,255,255)"> Atención</h2>
-    <h4>No existen registros en usuario</h4>
    
-    </div>
-     <?php 
-	}else{
+    
+      <?php 
+		
+		$SQL="SELECT nombre FROM usuarioproducto,producto where and  usuarioproducto.productoid=producto.productoid and usuarioid=".$_GET['id'];
+		$result = pg_query ($conn, $SQL ) or die("Error en la consulta SQL");
+		$registros= pg_num_rows($result);
 	
 	?>
-      <div class="well well-large">
+     <div class="span9 well well-large">
+  
         <p>
-				<table width="100%" class="footable table-striped table-hover" data-page-size="5">
-      <thead>
+        <?php
+		?>
+    <form method="post">
+    <?php
+    if($registros != 0){
+		
+		?>
+	 <table class="footable table-striped table-hover" data-page-size="5">
+			  <thead>
 				<tr>
 				  <th data-class="expand" data-sort-initial="true" data-type="numeric">
-					<span>Id</span>
+				
+					<span>Nombre</span>
 				  </th>
-				  <th><span>Nombre</span></th>
-                  <th><span>Apellido</span></th>
-				  <th data-hide="phone" data-sort-ignore="true">
+ <th data-hide="phone" data-sort-ignore="true">
 					<span class="add-on"> <i class="icon-pencil"></i> </span> Editar 
 				  </th>
-				  <th data-hide="phone" data-sort-ignore="true">
-				<span class="add-on"><i class="icon-trash"></i></span> Eliminar 
-				  </th>
-                  <th data-hide="phone" data-sort-ignore="true">
-				<span class="add-on"><i class="icon-eye-open"></i></span> Ver 
-				  </th>
                    <th data-hide="phone" data-sort-ignore="true">
-				<span class="add-on"><i class="icon-eye-open"></i></span> Asignar Productos 
+					<span class="add-on"> <i class="icon-trash"></i> </span> Eliminar
 				  </th>
 				</tr>
-	 	</thead>
- 		 <tbody>
-      <?php    
+			  </thead>
+				<tbody>
+      <?php 
 		for ($i=0;$i<$registros;$i++)
 			{
-
 			$row = pg_fetch_array ($result,$i );
-			echo '<tr>';
-			echo '<td width="10%">'.$row[0].'</td>';
-			echo '<td width="15%">'.$row[1].'</td>';
-			echo '<td width="15%">'.$row[2].'</td>';
-			echo '<td width="13%"><a href="editarusuario.php?id='.$row[0].'&boton=editar"> <button class="btn btn-primary"> <span class="add-on"><i class="icon-pencil"></i> </span> Editar  </button> </td>';
-			echo '<td width="13%"><a href="eliminarusuario.php?id='.$row[0].'&boton=eliminar"> <button class="btn btn-primary" type="button"  name="eliminar"> <span class="add-on"><i class="icon-trash"></i></span> Eliminar</button> </td>';
-			echo '<td width="11%"> <a href="verusuario.php?id='.$row[0].'&boton=ver"><button class="btn btn-primary"> <span class="add-on"><i class="icon-eye-open"></i></span> Ver</button> </td>';
-			echo '<td width="12%"> <a href="usuarioproducto.php?id='.$row[0].'&boton=ver"><button class="btn btn-primary"> <span class="add-on"><i class="icon-plus"></i></span> Asignar</button> </td>';
+		    echo '<tr>';
+			echo '<td width="40%">'.$row7["nombre"].' </td> </a>'; 
+			echo '<td width="15%"> <a href="editarmenu.php?id='.$row["menuid"].'&boton=editar"> <button class="btn btn-primary"  type="button" name="boton"> <span class="add-on"><i class="icon-pencil"></i> </span> Editar  </button>  </td></a>';
+			echo '<td width="15%">  <a href="eliminarmenu.php?id='.$row["menuid"].'&boton=eliminar"> <button class="btn btn-primary"  type="button"  name="boton"> <span class="add-on"><i class="icon-trash"></i> </span> Eliminar  </button>  </td></a>';
 			echo '</tr>';
 			}
 		?>
-		</tbody>	
-</table>
- <ul id="pagination" class="footable-nav"><span>Pages:</span></ul>
-
-<?php 
-	}
-?>
+        </tbody>	  
+    </table>
+      <div class="span12">   <ul id="pagination" class="footable-nav"><span>Pages:</span></ul></div>
+     <?php }else{  ?>
+		 <div class="well alert alert-block">
+   	<h2  align="center" style="color:rgb(255,255,255)"> Atención</h2>
+    <h4 align="center">No tiene productos asignados</h4>
+    </div>
 		 
-        
+		 
+	<?php } ?>
+     
+   </br>
+   </br>
+   <button type="button" class="btn btn-warning" data-toggle="collapse" data-target="#demo">
+    Presione aquí para asignar
+</button>
+ 
+<div id="demo" class="collapse">
+      <div class="row-fluid">
+            <dl class="dl-horizontal">
+              <dt>
+                <div class=" well well-small"><b>Nombre</b> del producto</div>
+              </dt>
+              <dd>
+                <div class=" well well-small">
+					<select id="producto" name="producto">
+                    	<option value="0">Seleccione Opción</option>
+                        <?php
+		
+						$SQL="SELECT * FROM producto where productoid NOT IN (SELECT productoid FROM usuarioproducto where usuarioid=".$_GET['id'].")";
+						$resultpro = pg_query ($conn, $SQL ) or die("Error en la consulta SQL");
+						
+						while($rowpro=pg_fetch_array($resultpro)){
+							echo '<option value="'.$rowpro['productoid'].'">'.$rowpro['nombre'].'</option>';
+							}
+						?>
+                    </select>
+                    </div>
+            </dd>
+              </dl>
+                <div align="center" class="well well-small"><button id="guardar" name="guardar" class="btn btn-primary text-center" type="submit"> Guardar</button>
+                 </div>
+       </div>        
+  </div>
+	 
+</form> 	
+    
+
+  </div>
+
+		
+
+           
         
          </p>
       </div>
-    </div>
+    
     </div>
   
 </div>
 
 <!-- Le javascript
 ================================================== --> 
+
 <script type="text/javascript" src="../recursos/js/jquery-2.0.2.js" ></script> 
 <script src="../recursos/js/bootstrap.js"></script> 
 
@@ -165,6 +218,5 @@ if(!isset($_SESSION["usuarioadmin"]) || !isset($_SESSION["passwordadmin"])){
       $('table').footable();
     });
   </script>
-
 	</body>
 </html>
